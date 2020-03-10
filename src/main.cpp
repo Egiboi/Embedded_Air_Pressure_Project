f914fdb2a7ba3ecd3b5a6ab833702238b55c2f01
Daniel Eggleton <43570890+Egiboi@users.noreply.github.com>
2020-03-10 11:22:00 +0200

    GUI classes added

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@
 // TODO: insert other include files here
 #include "DigitalIoPinMaster/DigitalIoPin.h"
 
-#include "..\i2c_class\I2C.h"
+#include "i2c_class/I2C.h"
 #if defined(BOARD_NXP_LPCXPRESSO_1549)
 /** 7-bit I2C addresses of  sensor (data sheet page 4/10) */
 #define I2C_PRES_ADDR  (0x40)
@@ -35,13 +35,48 @@ bool readPressureSensor(){
 	uint8_t commandRead = 0xF1;
 	presSens.write(I2C_PRES_ADDR, &commandRead, 1);
 	presSens.read(I2C_PRES_ADDR, data, 3);
+
+
 	int16_t dataCombined= data[0];
 	dataCombined = dataCombined << 8;
 	dataCombined |= data[1];
 	dataCombined = dataCombined *0.95/240;
-	printf("%d\n",dataCombined);
-	printf("%d\n",(int)data[2]);
-	return true;
+
+	bool CRCCheck=false;
+	uint16_t CRC = 0x131;
+	int32_t CRCdata=0x0;
+	CRCdata |= dataCombined;
+	CRCdata= CRCdata<<8;
+	int k=16+8;
+	while(k!=0){
+		if((CRCdata & ( 1 << k )) >> k == 1){
+			int tempk=k;
+			for(int n=8;n>0;n--){
+				int bit1 =(CRCdata & ( 1 << tempk )) >> tempk;
+				int bit2 =(CRC & (1<<n))>>n;
+				if((bit1==0 && bit2==0)||(bit1==1 && bit2==1)){
+					((1 << tempk) | n);
+				}
+				tempk--;
+			}
+		}
+		k--;
+
+	}
+	int mask= (1<<8)-1;
+	if((CRCdata & mask) == 0){
+		CRCCheck =true;
+		printf("%d\n",dataCombined);
+		printf("%d\n",(int)data[2]);
+	}else{
+		CRCCheck=false;
+		printf("Read fail\n");
+	}
+
+
+
+
+	return CRCCheck;
 
 }
 
