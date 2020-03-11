38a76d3e00d6833e5451e2ead06d2e1a2a6b097d
addei <47223277+addei@users.noreply.github.com>
2020-03-11 11:08:28 +0200

    Backend stuff

--- src/BackEnd/BackEnd.h
+++ src/BackEnd/BackEnd.h
@@ -13,9 +13,13 @@ public:
 	BackEnd();
 	virtual ~BackEnd();
 	bool setFrequency(ModbusMaster& node, uint16_t freq);
+	bool readPressureSensor();
+	int getPressureSensor();
 private:
-	ModbusRegister asd; //TODO väliaikainen, korjaa OIKEAAN MUOTOON
-	ModbusMaster ass; //TODO väliaikainen, korjaa muotoon referenssi
+	int pressure;
+	static const int current;
+	const uint16_t fa[20] = { 1000, 2000, 3000, 3500, 4000, 5000, 7000, 8000, 10000, 15000, 20000, 9000, 8000, 7000, 6000, 5000, 4000, 3000, 2000, 1000 };
+
 };
 
 #endif /* BACKEND_BACKEND_H_ */
