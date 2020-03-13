9715b47d3f5d534f59f20ede576841e8c91576cd
addei <47223277+addei@users.noreply.github.com>
2020-03-13 09:57:49 +0200

    more cleaning

--- Debug/src/GUI_class/MenuItem.d
+++
@@ -1,7 +0,0 @@
-src/GUI_class/MenuItem.o src/GUI_class/MenuItem.d: \
- ../src/GUI_class/MenuItem.cpp ../src/GUI_class/MenuItem.h \
- ../src/GUI_class/PropertyEdit.h
-
-../src/GUI_class/MenuItem.h:
-
-../src/GUI_class/PropertyEdit.h:
--- Debug/src/ModbusMaster/retarget_itm.d
+++
@@ -1,2 +0,0 @@
-src/ModbusMaster/retarget_itm.o src/ModbusMaster/retarget_itm.d: \
- ../src/ModbusMaster/retarget_itm.c
